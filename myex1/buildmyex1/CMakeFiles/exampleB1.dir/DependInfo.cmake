3b596f6983d91fd1c8fd6a147bf3805612a017b1
local1 <local1@localhost.localdomain>
2018-09-08 17:23:40 +0700

    It is a test commit

---
+++ myex1/buildmyex1/CMakeFiles/exampleB1.dir/DependInfo.cmake
@@ -0,0 +1,62 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/local1/CTauSim/myex1/B1/exampleB1.cc" "/home/local1/CTauSim/myex1/buildmyex1/CMakeFiles/exampleB1.dir/exampleB1.cc.o"
+  "/home/local1/CTauSim/myex1/B1/src/B1ActionInitialization.cc" "/home/local1/CTauSim/myex1/buildmyex1/CMakeFiles/exampleB1.dir/src/B1ActionInitialization.cc.o"
+  "/home/local1/CTauSim/myex1/B1/src/B1DetectorConstruction.cc" "/home/local1/CTauSim/myex1/buildmyex1/CMakeFiles/exampleB1.dir/src/B1DetectorConstruction.cc.o"
+  "/home/local1/CTauSim/myex1/B1/src/B1EventAction.cc" "/home/local1/CTauSim/myex1/buildmyex1/CMakeFiles/exampleB1.dir/src/B1EventAction.cc.o"
+  "/home/local1/CTauSim/myex1/B1/src/B1PrimaryGeneratorAction.cc" "/home/local1/CTauSim/myex1/buildmyex1/CMakeFiles/exampleB1.dir/src/B1PrimaryGeneratorAction.cc.o"
+  "/home/local1/CTauSim/myex1/B1/src/B1RunAction.cc" "/home/local1/CTauSim/myex1/buildmyex1/CMakeFiles/exampleB1.dir/src/B1RunAction.cc.o"
+  "/home/local1/CTauSim/myex1/B1/src/B1SteppingAction.cc" "/home/local1/CTauSim/myex1/buildmyex1/CMakeFiles/exampleB1.dir/src/B1SteppingAction.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4MULTITHREADED"
+  "G4UI_USE"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4UI_USE_XM"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "G4VIS_USE_OPENGLXM"
+  "G4_STORE_TRAJECTORY"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_PRINTSUPPORT_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/clhep/2.4.0.2/lib/CLHEP-2.4.0.2/../../include"
+  "/usr/local/geant4.10.04.p01/include/Geant4"
+  "/usr/local/XercesC/3.1.2/include"
+  "/home/local1/CTauSim/myex1/B1/include"
+  "/usr/local/clhep/2.4.0.2/include"
+  "/usr/include/qt5"
+  "/usr/include/qt5/QtWidgets"
+  "/usr/include/qt5/QtGui"
+  "/usr/include/qt5/QtCore"
+  "/usr/lib64/qt5/./mkspecs/linux-g++"
+  "/usr/include/qt5/QtPrintSupport"
+  "/usr/include/qt5/QtOpenGL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
